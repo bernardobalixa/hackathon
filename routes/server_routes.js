46fad9de0efad9a463edadd822524510edd990d8
Bernardo Balixa <bernardobalixa@gmail.com>
2023-12-06 10:45:21 +0000

    Api key adicionada

--- routes/server_routes.js
+++ routes/server_routes.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
 
 const router = express.Router();
+
+console.log(process.env.OPENAI_API_KEY);
 
 router.get("/abc", (req, res) => {
     
